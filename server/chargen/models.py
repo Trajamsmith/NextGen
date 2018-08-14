9b20c903dd0652c0a8e476b3288d75f312b67034
Travis James Smith <trajamsmith@gmail.com>
2018-08-14 10:35:11 -0500

    Add class proficiencies to class model

--- server/chargen/models.py
+++ server/chargen/models.py
@@ -16,9 +16,23 @@ class Race(models.Model):
 
 class Class(models.Model):
     name = models.CharField(max_length=30)
-    ability_modifiers = ArrayField(
+    armor_proficiencies = ArrayField(
         ArrayField(
-            models.IntegerField(default=0),
-            size=5,
+            models.CharField(max_length=10, blank=True),
+        )
+    )
+    weapon_proficiencies = ArrayField(
+        ArrayField(
+            models.CharField(max_length=10, blank=True),
+        )
+    )
+    tool_proficiencies = ArrayField(
+        ArrayField(
+            models.CharField(max_length=10, blank=True),
+        )
+    )
+    skill_proficiencies = ArrayField(
+        ArrayField(
+            models.CharField(max_length=10, blank=True),
         )
     )
